b340d41ab3347eaf9c815eb34c74617548d2a16e
tushergoon <tushergoondip@gmail.com>
2021-11-21 22:07:12 +0600

    redy to update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// connect to database
+// connect to database  from here
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9ihb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
